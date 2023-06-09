4ad543175dbe5983578af90a84ec12f420de17ab
bohemian-coder <jenniferejeme@gmail.com>
2023-06-08 22:58:24 -0500

    update security group, allow self

--- secgrp.tf
+++ secgrp.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "javapp-beanstalk-elb-sg" {
 
 resource "aws_security_group" "javapp-bastion-sg" {
     name = "javapp-bastion-sg"
-    description = "Security group for EC2 jump box"
+    description = "Security group for jump box instance"
     vpc_id = module.vpc.default_vpc_id
 
     egress {
@@ -40,7 +40,7 @@ resource "aws_security_group" "javapp-bastion-sg" {
 
 resource "aws_security_group" "javapp-beanstalk-prod-sg" {
     name = "javapp-beanstalk-prod-sg"
-    description = "Security group for EC2 instances"
+    description = "Security group for beanstalk instances"
     vpc_id = module.vpc.default_vpc_id
 
     egress {
@@ -56,4 +56,33 @@ resource "aws_security_group" "javapp-beanstalk-prod-sg" {
         protocol = "tcp"
         security_groups = [aws_security_group.javapp-bastion-sg.id]
     }
+}
+
+resource "aws_security_group" "javapp-backend-sg" {
+    name = "javapp-backend-sg"
+    description = "Security group for RDS, elastic cache and active MQ"
+    vpc_id = module.vpc.default_vpc_id
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        security_groups = [aws_security_group.javapp-beanstalk-prod-sg.id]
+    }
+}
+
+resource "aws_security_group_rule" "allow_access_from_self" {
+    type = "ingress"
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    security_group_id = aws_security_group.javapp-backend-sg.id
+    source_security_group_id = aws_security_group.javapp-backend-sg.id
 }
