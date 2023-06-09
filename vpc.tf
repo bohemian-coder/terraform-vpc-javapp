65e284b22c9f41ef080a13369146dddef23f2f03
bohemian-coder <jenniferejeme@gmail.com>
2023-06-08 22:10:29 -0500

    code format

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,19 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = var.VPC_NAME
-  cidr = var.VPC_CIDR_BlK
-  azs = [ var.ZONE1, var.ZONE2, var.ZONE3]
+  name            = var.VPC_NAME
+  cidr            = var.VPC_CIDR_BlK
+  azs             = [var.ZONE1, var.ZONE2, var.ZONE3]
   private_subnets = [var.privSub1_CIDR, var.privSub2_CIDR, var.privSub3_CIDR]
-  public_subnets = [var.pubSub1_CIDR, var.pubSub2_CIDR, var.pubSub3_CIDR]
+  public_subnets  = [var.pubSub1_CIDR, var.pubSub2_CIDR, var.pubSub3_CIDR]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
-    Terraform = true
+    Terraform   = true
     Environment = "Prod"
   }
 
