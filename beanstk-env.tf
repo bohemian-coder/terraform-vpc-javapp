333877f226f72fec3c8d953ef81526826050542a
bohemian-coder <jenniferejeme@gmail.com>
2023-06-13 10:36:22 -0500

    update beanstalk env settings

--- beanstk-env.tf
+++ beanstk-env.tf
@@ -4,56 +4,62 @@ resource "aws_elastic_beanstalk_environment" "javapp-bean-prod-env" {
   application         = aws_elastic_beanstalk_application.javapp-bean-prod.name
   solution_stack_name = "64bit Amazon Linux 2 v4.3.8 running Tomcat 8.5 Corretto 11"
   cname_prefix        = "javapp-bean-prod-domain"
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = module.vpc.default_vpc_id
-  }
-  setting {
+
+ setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
-    value     = "aws-elastickbeanstalk-ec2-role"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    name      = "VPCId"
+    namespace = "aws:ec2:vpc"
+    value     = module.vpc.vpc_id
   }
   setting {
     namespace = "aws:ec2:vpc"
     name      = "AssociatePublicIpAddress"
-    value     = false
+    value     = "false"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]])
   }
   setting {
     namespace = "aws:ec2:vpc"
-    name      = "subnets"
+    name      = "ELBSubnets"
     value     = join(",", [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]])
   }
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "ELBsubnets"
-    value     = join(",", [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]])
-  }
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t2.micro"
   }
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "EC2KeyName"
     value     = aws_key_pair.javappkey.key_name
   }
+
   setting {
     namespace = "aws:autoscaling:asg"
-    name      = "Availability Zone"
+    name      = "Availability Zones"
     value     = "Any 3"
   }
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
-    value     = 1
+    value     = "1"
   }
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
-    value     = 6
+    value     = "8"
   }
+
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "environment"
@@ -67,10 +73,10 @@ resource "aws_elastic_beanstalk_environment" "javapp-bean-prod-env" {
   setting {
     namespace = "aws:elasticbeanstalk:healthreporting:system"
     name      = "SystemType"
-    value     = "basic"
+    value     = "enhanced"
   }
   setting {
-    namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
     name      = "RollingUpdateEnabled"
     value     = "true"
   }
@@ -79,6 +85,7 @@ resource "aws_elastic_beanstalk_environment" "javapp-bean-prod-env" {
     name      = "RollingUpdateType"
     value     = "Health"
   }
+
   setting {
     namespace = "aws:autoscaling:updatepolicy:rollingupdate"
     name      = "MaxBatchSize"
@@ -89,16 +96,19 @@ resource "aws_elastic_beanstalk_environment" "javapp-bean-prod-env" {
     name      = "CrossZone"
     value     = "true"
   }
+
   setting {
     name      = "StickinessEnabled"
     namespace = "aws:elasticbeanstalk:environment:process:default"
     value     = "true"
   }
+
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "BatchSizeType"
     value     = "Fixed"
   }
+
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "BatchSize"
@@ -109,11 +119,13 @@ resource "aws_elastic_beanstalk_environment" "javapp-bean-prod-env" {
     name      = "DeploymentPolicy"
     value     = "Rolling"
   }
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "SecurityGroups"
     value     = aws_security_group.javapp-beanstalk-prod-sg.id
   }
+
   setting {
     namespace = "aws:elbv2:loadbalancer"
     name      = "SecurityGroups"
